6edb09356e47665a56a5b04c1919cba7f349a996
Nij <nijdave114@gmail.com>
2026-03-31 15:45:39 +0530

    fix: python version and template path for vercel

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,12 @@
 import sys
 import os
+
 sys.path.insert(0, os.path.dirname(__file__))
+os.environ['PYTHONPATH'] = os.path.dirname(__file__)
+
 from src.app import app
+
+app.template_folder = os.path.join(os.path.dirname(__file__), 'src', 'templates')
 
 if __name__ == "__main__":
     app.run()
